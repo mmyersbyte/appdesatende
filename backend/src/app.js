5a536f6d14bdb2e2df0fad21f3b7d508511983ba
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-04-29 20:52:02 -0300

    Padronização mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,9 +18,6 @@ app.use('/api/empresas', empresaRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-connectDB(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB conectado');
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-  })
-  .catch((err) => console.error('Erro ao conectar no MongoDB:', err));
+connectDB(process.env.MONGODB_URI);
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,16 @@
 import mongoose from 'mongoose';
 
 export default function connectDB(uri) {
+  mongoose.connection.on('connected', () => {
+    console.log('MongoDB conectado com sucesso!');
+  });
+  mongoose.connection.on('error', (error) => {
+    console.error('Erro ao conectar no MongoDB:', error);
+    process.exit(1);
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.warn('MongoDB desconectado!');
+  });
   return mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
